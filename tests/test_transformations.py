e15e560c7c7a5f4717dc1adac8ef1bbbd81cbd2f
mvillasante <mario.villasante@ciencias.unam.mx>
2025-06-22 01:43:14 +0000

    🛑🧪 Fail tests

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,8 +1,10 @@
 from cocijo.plot_series import plot_series
 
 import pandas as pd
+import matplotlib
 
 
 def test_plot_series():
     data = pd.read_csv("data/registro_lluvias.csv")
-    plot_series(data)
+    obtained = plot_series(data)
+    assert isinstance(obtained, matplotlib.lines.Line2D)
