5300c4b97ab96295b9ed06adb2b0aa3ccf91ca80
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 13:58:37 +0000

    ♻️  Refactor

--- tests/test_plot_series.py
+++ tests/test_plot_series.py
@@ -23,6 +23,6 @@ def test_plot_series():
 
 def test_plot_cumulative():
     obtained = plot_cumulative(data)
-    # obtained_first_line = obtained.get_lines()[0]
-    # assert obtained_first_line.get_data()[1] > data.mm.max()
+    obtained_first_line = obtained.get_lines()[0]
+    assert max(obtained_first_line.get_data()[1]) > data.mm.max()
     matplotlib.pyplot.savefig("lluvia_acumulada.png")
