44d53f800fa54d1aee4abbdebd96b9fca729a89d
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 13:22:57 +0000

    ♻️  Refactor

--- cocijo/plot_series.py
+++ cocijo/plot_series.py
@@ -2,13 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_series(data):
+def plot_series(raw_data):
+    data = setup_data(raw_data)
     fig, ax = plt.subplots()
-    data["Fecha"] = pd.to_datetime(data["Fecha"], format="%Y-%m-%d")
-    data["MesDia"] = data["Fecha"].dt.strftime("%m-%d")
-    data.set_index("Fecha", inplace=True)
-    full_date = pd.date_range(start=data.index.min(), end=data.index.max())
-    data = data.reindex(full_date)
     ax.plot(data.mm, marker="o", linestyle="")
     labels = [
         pd.to_datetime(label.get_text(), format="%Y-%m").month_name()
@@ -20,3 +16,13 @@ def plot_series(data):
     plt.ylabel("Lluvia díaria (mm)")
     plt.tight_layout()
     return ax
+
+
+def setup_data(raw_data):
+    data = raw_data.copy()
+    data["Fecha"] = pd.to_datetime(data["Fecha"], format="%Y-%m-%d")
+    data["MesDia"] = data["Fecha"].dt.strftime("%m-%d")
+    data.set_index("Fecha", inplace=True)
+    full_date = pd.date_range(start=data.index.min(), end=data.index.max())
+    data = data.reindex(full_date)
+    return data
