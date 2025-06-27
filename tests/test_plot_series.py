15081bb8f3941e87e1d93b1a75416f676c9fa553
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 13:09:39 +0000

    🛑🧪 Fail tests

--- tests/test_plot_series.py
+++ tests/test_plot_series.py
@@ -13,6 +13,5 @@ def test_plot_series():
     assert obtained.get_ylabel() == "Lluvia díaria (mm)"
     obtained_first_line = obtained.get_lines()[0]
     assert obtained_first_line.get_marker() == "o"
-    assert len(obtained_first_line.get_data()[0]) == 12
-    expected_first_date = "January"
+    expected_first_date = "March"
     assert obtained.get_xticklabels()[0].get_text() == expected_first_date
