1ab83e34760c8f7b746834c9f06401a20d125198
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 03:35:43 +0000

    ♻️  Refactor

--- cocijo/plot_series.py
+++ cocijo/plot_series.py
@@ -7,7 +7,7 @@ def plot_series(data):
     data["Fecha"] = pd.to_datetime(data["Fecha"], format="%Y-%m-%d")
     data["MesDia"] = data["Fecha"].dt.strftime("%m-%d")
     data.set_index("MesDia", inplace=True)
-    full_dates = pd.date_range(start="1899-12-31", end="1900-12-01", freq="M")
+    full_dates = pd.date_range(start="1900-01-01", end="1900-12-01", freq="MS")
     data = data.reindex(full_dates.strftime("%m-%d"), fill_value=0)
 
     ax.plot(full_dates, data.mm, marker="o")
