1828b113ef0be257bb263043d3e0aeb1abd12548
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 03:26:37 +0000

    ✅ Pass tests

--- cocijo/plot_series.py
+++ cocijo/plot_series.py
@@ -5,7 +5,12 @@ import pandas as pd
 def plot_series(data):
     fig, ax = plt.subplots()
     data["Fecha"] = pd.to_datetime(data["Fecha"], format="%Y-%m-%d")
-    ax.plot(data.Fecha, data.mm, marker="o")
+    data["MesDia"] = data["Fecha"].dt.strftime("%m-%d")
+    data.set_index("MesDia", inplace=True)
+    full_dates = pd.date_range(start="1900-01-01", end="1900-12-31", freq="M")
+    data = data.reindex(full_dates.strftime("%m-%d"), fill_value=0)
+
+    ax.plot(full_dates, data.mm, marker="o")
     plt.xticks(rotation=90)
     plt.ylabel("Lluvia díaria (mm)")
     plt.tight_layout()
