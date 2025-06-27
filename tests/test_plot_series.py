140bb10ae9a045cd375601df7a18e1870ed07545
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 13:27:02 +0000

    ♻️  Refactor

--- tests/test_plot_series.py
+++ tests/test_plot_series.py
@@ -9,10 +9,11 @@ def test_plot_series():
     obtained = plot_series(data)
     assert isinstance(obtained, matplotlib.axes._axes.Axes)
     assert obtained.get_xticklabels()[1].get_rotation() == 90
-    matplotlib.pyplot.savefig("prueba.png")
     assert obtained.get_ylabel() == "Lluvia díaria (mm)"
     obtained_first_line = obtained.get_lines()[0]
     assert obtained_first_line.get_marker() == "o"
     expected_first_date = "March"
     assert obtained.get_xticklabels()[0].get_text() == expected_first_date
     assert obtained_first_line.get_linestyle() is "None"
+
+    matplotlib.pyplot.savefig("lluvia_diaria.png")
