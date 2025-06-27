298e227e1470305d8c886b742edac2d22b1084aa
Mario VB <mario.villasante@islas.org.mx>
2025-06-27 13:19:47 +0000

    🛑🧪 Fail tests

--- tests/test_plot_series.py
+++ tests/test_plot_series.py
@@ -15,4 +15,4 @@ def test_plot_series():
     assert obtained_first_line.get_marker() == "o"
     expected_first_date = "March"
     assert obtained.get_xticklabels()[0].get_text() == expected_first_date
-    assert obtained_first_line.get_linestyle() is None
+    assert obtained_first_line.get_linestyle() is "None"
