dbd0e30b5930e1cd946e262795d5398968e1bd18
Mario VB <mario.villasante@islas.org.mx>
2025-08-01 13:34:32 +0000

    ♻️  Refactor

--- cocijo/plot_series.py
+++ cocijo/plot_series.py
@@ -42,9 +42,6 @@ def setup_data(raw_data):
     data = raw_data.copy()
     data["Fecha"] = pd.to_datetime(data["Fecha"], format="%Y-%m-%d")
     data["dia_del_año"] = data["Fecha"].dt.dayofyear
-    data.set_index("Fecha", inplace=True, drop=False)
-    full_date = pd.date_range(start=data.index.min(), end=data.index.max())
-    data = data.reindex(full_date, fill_value=0)
     return data
 
 
