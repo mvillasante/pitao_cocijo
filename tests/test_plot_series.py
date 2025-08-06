f5af4896211fc4ebd7172d49521257d8a1a06500
Mario VB <mario.villasante@islas.org.mx>
2025-08-06 14:58:50 +0000

    ♻️  Refactor

--- tests/test_plot_series.py
+++ tests/test_plot_series.py
@@ -3,17 +3,13 @@ from cocijo.plot_series import plot_series, plot_cumulative, setup_data, plot_ye
 import pandas as pd
 import matplotlib
 
-import pytest
-
 
 def test_plot_2024():
     data = pd.read_csv("data/registro_lluvias.csv")
-    plot_series(data)
+    plot_yearly_rain(data)
     matplotlib.pyplot.savefig("lluvia_diaria.png")
     plot_cumulative(data)
     matplotlib.pyplot.savefig("lluvia_acumulada.png")
-    plot_yearly_rain(data)
-    matplotlib.pyplot.savefig("lluvia_por_año.png")
 
 
 data = pd.read_csv("tests/data/registro_lluvias_for_test.csv")
