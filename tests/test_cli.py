b350c12e765119dc44579b3df9fdc5c640ed1fcd
Mario VB <mario.villasante@ciencias.unam.mx>
2026-04-04 02:02:20 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,9 +4,12 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
-def test_app_write_monthly_summary():
-    result = runner.invoke(
-        app,
-    )
+def test_write_daily_rain():
+    result = runner.invoke(app, ["write-daily-rain-plot", "--help"])
+    assert result.exit_code == 0
+
+
+def test_version():
+    result = runner.invoke([app, "--version"])
     assert result.exit_code == 0
     assert "0.1.0" in result.stdout
