8e5b93365be9a96855dfed79bf209c9382b941a5
Mario VB <mario.villasante@ciencias.unam.mx>
2026-04-04 02:07:12 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,13 @@ runner = CliRunner()
 def test_write_daily_rain():
     result = runner.invoke(app, ["write-daily-rain-plot", "--help"])
     assert result.exit_code == 0
+    data_path = "tests/data/registro_lluvias_for_test.csv"
+    output_path = "tests/data/lluvia_diaria.png"
+
+    result = runner.invoke(
+        app, ["write-daily-rain-plot", "data-path", data_path, "output-path", output_path]
+    )
+    assert result.exit_code == 0
 
 
 def test_version():
