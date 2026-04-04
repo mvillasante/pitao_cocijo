06cdce52ede1968ec51ce78e153bccea0a00ac3a
Mario VB <mario.villasante@ciencias.unam.mx>
2026-04-04 02:16:28 +0000

    ✅ Pass tests

--- cocijo/cli.py
+++ cocijo/cli.py
@@ -1,12 +1,18 @@
+from cocijo.plot_series import plot_yearly_rain
 import cocijo
+
+import matplotlib
+import pandas as pd
 import typer
 
 app = typer.Typer()
 
 
 @app.command()
-def write_daily_rain_plot():
-    pass
+def write_daily_rain_plot(data_path: str = typer.Option(), output_path: str = typer.Option()):
+    data = pd.read_csv(data_path)
+    plot_yearly_rain(data)
+    matplotlib.pyplot.savefig(output_path)
 
 
 @app.command()
