d1d1e1664fa8ef5ff1d024686cc6d5d8107e07c5
Mario VB <mario.villasante@ciencias.unam.mx>
2026-04-04 02:19:57 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,20 @@ import os
 runner = CliRunner()
 
 
+def test_write_cumulative_rain():
+    result = runner.invoke(app, ["write-cumulative-rain-plot", "--help"])
+    assert result.exit_code == 0
+    data_path = "tests/data/registro_lluvias_for_test.csv"
+    output_path = "tests/data/lluvia_acumulada.png"
+
+    if_exist_remove(output_path)
+    result = runner.invoke(
+        app, ["write-cumulative-rain-plot", "--data-path", data_path, "--output-path", output_path]
+    )
+    assert result.exit_code == 0
+    assert os.path.isfile(output_path)
+
+
 def test_write_daily_rain():
     result = runner.invoke(app, ["write-daily-rain-plot", "--help"])
     assert result.exit_code == 0
