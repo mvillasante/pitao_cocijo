e38b85ce098098c126459fd61b314542f57c78bb
Mario VB <mario.villasante@ciencias.unam.mx>
2026-04-04 02:20:51 +0000

    ✅ Pass tests

--- cocijo/cli.py
+++ cocijo/cli.py
@@ -1,4 +1,4 @@
-from cocijo.plot_series import plot_yearly_rain
+from cocijo.plot_series import plot_yearly_rain, plot_cumulative_yearly_rain
 import cocijo
 
 import matplotlib
@@ -6,6 +6,13 @@ import pandas as pd
 import typer
 
 app = typer.Typer()
+
+
+@app.command()
+def write_cumulative_rain_plot(data_path: str = typer.Option(), output_path: str = typer.Option()):
+    data = pd.read_csv(data_path)
+    plot_cumulative_yearly_rain(data)
+    matplotlib.pyplot.savefig(output_path)
 
 
 @app.command()
